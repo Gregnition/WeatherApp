64391ef42c82e491f234ef838d26f75f500ef420
Greg Harris <gregtastik@gmail.com>
2025-09-30 12:09:40 -0500

    Switch weather function to native fetch

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export async function handler(event) {
   const { city, lat, lon, forecast } = event.queryStringParameters || {};
   const key = process.env.OPENWEATHER_KEY;
